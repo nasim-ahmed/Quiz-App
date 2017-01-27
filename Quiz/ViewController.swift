048086a30053c8e06350e1915ff2956c0830af43
NASIM AHMED <nasim.ahmed54@gmail.com>
2017-01-27 12:13:00 +0600

    first commit

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -9,16 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentIndex:Int = 0
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    
+    let questionArray:[String] = ["WHAT IS THE CAPITAL OF BANGLADESH?", "WHO IS THE FOUNDER OF FACEBOOK?","WHAT IS THE CAPITAL OF RUSSIA?","FULL FORM OF WHO?"]
+    
+    
+    let answerArray:[String] = ["DHAKA","MARK ZUCKERBERG","MOSCOW","WORLD HEALTH ORGANIZATION"]
+    
+    
+    
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+        
+        currentIndex += 1
+       
+        if currentIndex == questionArray.count{
+            currentIndex = 0
+        }
+        let question:String = questionArray[currentIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+        
+        
+    }
+    
+    @IBAction func showAnswer(_ sender: UIButton) {
+        
+        let answer:String = answerArray[currentIndex]
+        answerLabel.text = answer
+
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        questionLabel.text = questionArray[currentIndex]
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+   
 
 
 }
